52fc65a1686d683e45237f809eb3f8c09d4cf249
Jacob Huff <jacobhuff1313@gmail.com>
2021-09-15 13:55:13 -0400

    build attempt#2

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// Production Mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join((__dirname = 'client/build/index.html')));
+  });
+}
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 app.listen(PORT, () => {
