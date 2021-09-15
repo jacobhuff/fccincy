2842751dbe3e74efcc884476259faff6b140a1c8
Jacob Huff <jacobhuff1313@gmail.com>
2021-09-15 13:23:47 -0400

    heroku build

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Production Mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join((__dirname = 'client/build/index.html')));
+  });
+}
+
+// Build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
